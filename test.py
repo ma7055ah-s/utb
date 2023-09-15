af6a126beb8cfc211ab97fbfb2bd569d05a70340
ma7055ah-s <75044517+ma7055ah-s@users.noreply.github.com>
2023-09-15 16:03:24 +0200

    test2

---
+++ test.py
@@ -0,0 +1,10 @@
+"""
+This module contains example constants.
+"""
+
+print("Hello world.")
+VAR1=1
+VAR2="three"
+var3=VAR1 + 2
+print("Run this programming in debugging mode and set a breakpoint on the next line.")
+input("Press enter to quit the program.")
